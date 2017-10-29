37cdc47c0a7c02b9b3fbd52583df9d2c69a1995e
Bjorn Wickstrom <bjorn@billo.systems>
2017-10-29 09:16:38 -0500

    Updated artifact ids.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject clj-xbee-example "0.1.0-SNAPSHOT"
+(defproject systems.billo/clj-xbee-example "0.1.0-SNAPSHOT"
   :description "An Example Application Built with the clj-xbee Wrapper Library"
   :url "https://github.com/billosys/clj-xbee-example"
   :license {
     :name "Apache License Version 2.0"
     :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [
-    [clj-xbee "0.1.0-SNAPSHOT"]
+    [systems.billo/clj-xbee "0.1.0-SNAPSHOT"]
     [org.clojure/clojure "1.8.0"]])
