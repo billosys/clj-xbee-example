60debbed29af72905f7737f142fbe9f105e78f6e
Bjorn Wickstrom <bjorn@billo.systems>
2017-10-30 00:28:05 -0500

    Added a main func.

--- src/xbee_example/core.clj
+++ src/xbee_example/core.clj
@@ -1,6 +1,20 @@
-(ns xbee-example.core)
+(ns xbee-example.core
+  (:require [clojusc.twig :as logger]
+            [xbee.device.xbee :as xbee]
+            [xbee.models.core :as models])
+  (:import (com.digi.xbee.api.exceptions XBeeException))
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn -main
+  ([]
+    (-main "COM1"))
+  ([port]
+    (-main port "9600"))
+  ([port baud-rate]
+    (-main port baud-rate "This is a test message."))
+  ([port baud-rate msg]
+    (logger/set-level! '[xbee xbee-example com.digi] :info)
+    (let [device (xbee/create-device port baud-rate)]
+      (xbee/open device)
+      (xbee/send-broadcast-data device msg)
+      (xbee/close device))))
