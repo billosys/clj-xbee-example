e65b115da43a0be3debf7780e51e63dce64642e2
Bjorn Wickstrom <bjorn@billo.systems>
2017-10-30 00:27:30 -0500

    Added coloured logging.

--- project.clj
+++ project.clj
@@ -5,6 +5,7 @@
     :name "Apache License Version 2.0"
     :url "https://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [
+    [clojusc/twig "0.3.2"]
     [org.clojure/clojure "1.8.0"]
     [systems.billo/clj-xbee "0.1.0-SNAPSHOT"]]
   :main xbee-example.core
